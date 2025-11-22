1c574cf4f5f8335d9fa75113b469c4b074bf8395
Sandra Prieto <jprietoc@unal.edu.co>
2025-11-22 20:25:18 +0100

    Some layout for gamepage to start being implemented

---
+++ checkers-game/src/components/game/gamepage/gamepage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gamepage',
+  imports: [],
+  templateUrl: './gamepage.html',
+  styleUrl: './gamepage.css'
+})
+export class Gamepage {
+
+}
